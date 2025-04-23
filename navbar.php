047e3f182570df9734e1133b82cad764a4a7f419
FunsizedWrath <151151740+FunsizedWrath@users.noreply.github.com>
2025-04-23 12:27:34 +0200

    feat logout works + fix navbar display

--- navbar.php
+++ navbar.php
@@ -11,25 +11,27 @@
         <a href="index.php">Accueil</a>
         <a href="library.php">Bibliothèque</a>
         <?php
-                // Start the session at the very beginning of the script
-                session_start();
+        // Start the session at the very beginning of the script
+        session_start();
 
-                // Debugging: Ensure session variables are set correctly
-                if (!isset($_SESSION['role'])) {
-                    error_log('$_SESSION["role"] is not set.');
-                } else {
-                    error_log('$_SESSION["role"]: ' . $_SESSION['role']);
-                }
-        if (isset($_SESSION['user_id'])) {
-            echo '<a href="account.php">Compte</a>';
-            echo 'a href="logout.php">Déconnexion</a>';
+        // Debugging: Ensure session variables are set correctly
+        if (!isset($_SESSION['role'])) {
+            error_log('$_SESSION["role"] is not set.');
         } else {
-            echo '<a href="login.php">Se connecter/Créer un compte</a>';
+            error_log('$_SESSION["role"]: ' . $_SESSION['role']);
         }
         if (isset($_SESSION['role']) && in_array($_SESSION['role'], [1, 2, 3])) {
             echo '<a href="manage_library.php">Gérer la bibliothèque</a>';
             echo '<a href="manage_clients.php">Gérer les clients</a>';
         }
+        echo '<div class="navbar-right">';
+        if (isset($_SESSION['user_id'])) {
+            echo '<a href="account.php">Compte</a>';
+            echo '<a href="logout.php">Déconnexion</a>';
+        } else {
+            echo '<a href="login.php">Se connecter/Créer un compte</a>';
+        }
+        echo '</div>';
     ?>
     </div>
 </body>
